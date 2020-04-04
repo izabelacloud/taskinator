957a1d9a2c63e254c94fc67e673dbd222774e5b4
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-04-04 11:00:00 -0700

    code reaftor for how to create a content of tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,45 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 var createTaskHandler = function(event) {
-    event.preventDefault();
 
+    //original code
+
+    event.preventDefault();
+    // console.log(event);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskNameInput);
+    console.dir(taskNameInput);
+    console.log(taskTypeInput);
+
+    // var listItemEl = document.createElement("li");
+    // listItemEl.className = "task-item";
+    // // listItemEl.textContent = "This is a new task.";
+    // listItemEl.textContent = taskNameInput;
+    // tasksToDoEl.appendChild(listItemEl);
+
+    //end of original code
+
+    //new code
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give it a class name
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
+
+
+
 }
 
 // buttonEl.addEventListener("click", createTaskHandler);
